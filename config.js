7c1177875e3806896e940956392ceb16c8542eec
Yaroslav <petrushenkoia11@gmail.com>
2026-04-07 20:51:22 +0600

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   DATABASE_URL: process.env.DATABASE_URL,
   ANYPAY_API_KEY: process.env.ANYPAY_API_KEY,
-  ANYPAY_SHOP_ID: process.env.ANYPAY_SHOP_ID
+  ANYPAY_SHOP_ID: process.env.ANYPAY_SHOP_ID,
+  WEBHOOK_URL: process.env.WEBHOOK_URL,
+
 };
