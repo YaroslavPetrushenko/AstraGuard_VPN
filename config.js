bf274b6911475b8531fda36e6536bab542c9ba74
Yaroslav <petrushenkoia11@gmail.com>
2026-04-08 20:46:36 +0600

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ module.exports = {
     ANYPAY: {
         PROJECT_ID: 17515,                 // ID проекта (магазина)
         API_ID: "6UQFFQBVEOTVG5ZO8U",      // API ID
-        API_KEY: "nuEz9Z...ОБРЕЖЬ_ТУТ..."  // В КОД НЕ ПИШИ, ВЫНЕСИ В ENV
+        API_KEY: "qKvwffyyEGEkGqf6yPptQy4zY4LdFKL26M0rAvE"  // В КОД НЕ ПИШИ, ВЫНЕСИ В ENV
     },
 
     TRIAL_HOURS: 24,
